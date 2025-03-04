cbdbc4111ad152997e6be217c900a4f0daf17c7b
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-03-04 18:06:33 +0530

    inject AutoMapper into program.cs

--- Student Course Management/Student Course Management/Program.cs
+++ Student Course Management/Student Course Management/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(Program));
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
