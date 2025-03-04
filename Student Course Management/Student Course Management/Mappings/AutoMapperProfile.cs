9413585cfedcbd2b1fdc38c8914ee3dc9d6afa6a
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-03-04 18:05:36 +0530

    AutoMapper

---
+++ Student Course Management/Student Course Management/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using Student_Course_Management.DTOs;
+using Student_Course_Management.Models;
+using AutoMapper;
+
+namespace Student_Course_Management.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Student, StudentDto>()
+                .ForMember(dest => dest.Courses, opt => opt.MapFrom(src => src.Courses.Select(c => c.Title)));
+            CreateMap<StudentDto, Student>();
+        }
+    }
+}
--- Student Course Management/Student Course Management/obj/Debug/net8.0/Student Course Management.AssemblyInfo.cs
+++ Student Course Management/Student Course Management/obj/Debug/net8.0/Student Course Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student Course Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7bd7962e96a167cfe6f8d57d3d518de7459ac70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3138727f7c70ba284bbc10e47690bdeeff0ecea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student Course Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student Course Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
